99666657f0e6e82d8ab6c0cf5f75dfc37fe4a4e8
jiangsir404 <2461805286@qq.com>
2020-03-01 21:32:24 +0800

    [ADD] 添加对常见排序算法(冒泡，插入/选择)的笔记总结

--- sort/insert_sort.py
+++ sort/insert_sort.py
@@ -8,6 +8,10 @@
 1. 首先，我们将数组中的数据分为两个区间，已排序区间和未排序区间。初始已排序区间只有一个元素，就是数组的第一个元素。
 2. 取未排序区间中的元素，在已排序区间中找到合适的插入位置将其插入
 3. 重复这个过程，直到未排序区间中元素为空，算法结束。
+
+记忆口诀:
+	划分左已排序区间和右未排序区间,range(1,len(nums))->range(i-1,-1,-1) 第一次遍历空出第一个元素，否则j=i-1就会溢出
+	找到插入点并进行数据搬移: if nums[j] > value:nums[j+1]=nums[j] else:break(找到插入点退出)
 """
 
 def insert_sort(nums):
@@ -18,7 +22,7 @@ def insert_sort(nums):
 		j = i - 1
 		# 查找插入位置
 
-		for j in range(i-1, -2, -1):
+		for j in range(i-1, -1, -1):
 			if nums[j] > value:
 				nums[j+1] = nums[j] # 数据往后搬移
 			else:
